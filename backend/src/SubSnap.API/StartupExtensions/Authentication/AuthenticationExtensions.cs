6bedc416a35067dd183866a1e45f29c3cf7d68c3
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-02-06 19:08:17 +0100

    update

--- backend/src/SubSnap.API/StartupExtensions/Authentication/AuthenticationExtensions.cs
+++ backend/src/SubSnap.API/StartupExtensions/Authentication/AuthenticationExtensions.cs
@@ -9,31 +9,34 @@ public static class AuthenticationExtensions
     public static IServiceCollection AddAuthenticationConfiguration(
         this IServiceCollection services, IConfiguration configuration)
     {
-        var jwtSettings = configuration.GetSection("JwtSettings");
-        var issuer = jwtSettings["Issuer"]!;
-        var audience = jwtSettings["Audience"]!;
-        var secretKey = jwtSettings["SecretKey"]!;
+        var jwtSettings = configuration.GetSection("JwtSettings");  //prende la section
+        var issuer = jwtSettings["Issuer"]!;  //chi ha emesso il token
+        var audience = jwtSettings["Audience"]!;  //a chi è destinato
+        var secretKey = jwtSettings["SecretKey"]!;  //firmare e verificare il token
 
         //e poi la connection str la prendi in .Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
 
         services
-            .AddAuthentication(options =>
+            .AddAuthentication(options =>  //"Questo progetto usa JWT Bearer Authentication"
             {
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    //quando aspnet deve auntenticare una richiesta -> usa JWT
                 options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    //quando serve risposndere w 401 unauthorized -> usa JWT
             })
             .AddJwtBearer(options =>
             {
-                options.TokenValidationParameters = new TokenValidationParameters
+                options.TokenValidationParameters = new TokenValidationParameters //definisci le regole di validazione dei token
                 {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
+                    ValidateIssuer = true,  //activate (il token deve essere emesso da chi dici tu)
+                    ValidateAudience = true,  //activate
+                    ValidateLifetime = true,  //non deve essere un token scaduto
                     ValidateIssuerSigningKey = true,
-                    ValidIssuer = jwtSettings["Issuer"],
-                    ValidAudience = jwtSettings["Audience"],
+                    ValidIssuer = jwtSettings["Issuer"], //devono combaciare perfettamente
+                    ValidAudience = jwtSettings["Audience"], //devono combaciare perfettamente
                     IssuerSigningKey = new SymmetricSecurityKey(
                         Encoding.UTF8.GetBytes(secretKey))
+                        //key simmetrica, deve essere la stessa usata quando generi il token
                 };
             });
         return services;
