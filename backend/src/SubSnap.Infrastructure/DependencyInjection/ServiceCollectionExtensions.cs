37991712f5971a0938f4debca96c142fea284077
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-02-08 10:53:29 +0100

    update

--- backend/src/SubSnap.Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
+++ backend/src/SubSnap.Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
@@ -7,6 +7,7 @@ using SubSnap.Core.Contracts.Services;
 using SubSnap.Core.Contracts.UnitOfWork;
 using SubSnap.Core.Services.Application;
 using SubSnap.Infrastructure.Identity.Services;
+using SubSnap.Infrastructure.Persistence.Context;
 using SubSnap.Infrastructure.Persistence.UnitOfWork;
 using SubSnap.Infrastructure.Repositories.Implementations;
 
@@ -15,10 +16,11 @@ namespace SubSnap.Infrastructure.DependencyInjection;
 public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddInfrastructure(  // custom extension x IServiceCollection (la collezione di servizi della DI di ASP.NET Core)
-        this IServiceCollection services,
-        IConfiguration configuration)
+        this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer( configuration.GetConnectionString("sqlConnection")));
+        //services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer( configuration.GetConnectionString("sqlConnection")));
+        services.AddDbContext<ApplicationDbContext>( options => options.UseNpgsql( configuration.GetConnectionString("sqlConnection") )
+        );  //postgreSQL x EF core
 
         //registrazione dei repositories!!
         //quando qualcuno chiede IUserRepository, la DI darà un’istanza concreta di UserRepository
@@ -30,5 +32,7 @@ public static class ServiceCollectionExtensions
         services.AddScoped<AuthService>();
 
         return services;
+        //scoped: una nuova istanza per ogni richiesta HTTP, condivisa all’interno della stessa richiesta. Perfetto x DbContext e servizi che lavorano con esso.
+        //!!!in questo modo quando chiami l'interfaccia, utilizzi invece la classe concrete. ottimo xk rispetti i BOUNDARIES(confini) tra prj, un proj non conosce le implementazioni dell'altro, ma SOLO LE INTERFACCIE. e poi xk ti permette di fare facilmente il mock dei servizi nelle unit test!!
     }
 }
