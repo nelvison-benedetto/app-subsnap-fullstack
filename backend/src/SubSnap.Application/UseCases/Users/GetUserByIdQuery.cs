797ba58ba73ecc31223b4623ee51e26ce2f77433
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-02-25 11:47:46 +0100

    perfect acrhitecture .core .application .infrastructure .API

--- backend/src/SubSnap.Application/UseCases/Users/GetUserByIdQuery.cs
+++ backend/src/SubSnap.Application/UseCases/Users/GetUserByIdQuery.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SubSnap.Application.UseCases.Users.GetUserById;
+namespace SubSnap.Application.UseCases.Users;
 
 //queries solo lettura 
 public sealed record GetUserByIdQuery(int UserId);
