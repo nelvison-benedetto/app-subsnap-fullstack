a3153ca0657c96a12db5d73c5662dbdbf311a339
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-02-26 20:10:57 +0100

    update

--- backend/src/SubSnap.API/Controllers/V1/AuthController.cs
+++ backend/src/SubSnap.API/Controllers/V1/AuthController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using SubSnap.API.Contracts.Auth;
 using SubSnap.API.Contracts.Responses;
-using SubSnap.Application.UseCases.Auth;
 using SubSnap.Core.Domain.ValueObjects;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
