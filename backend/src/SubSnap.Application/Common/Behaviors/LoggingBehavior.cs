1d450655e09822c5c66d07e171b52529f9d10f58
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-02-28 16:38:59 +0100

    update

--- backend/src/SubSnap.Application/Common/Behaviors/LoggingBehavior.cs
+++ backend/src/SubSnap.Application/Common/Behaviors/LoggingBehavior.cs
@@ -1,11 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
 
 namespace SubSnap.Application.Common.Behaviors;
 
-internal class LoggingBehavior
+/*
+ * ora ogni volta che un handler viene usato nel log appare 
+        Handling RegisterUserCommand
+        Handled RegisterUserCommand in 45ms
+ * 
+ services.AddTransient(
+    typeof(IPipelineBehavior<,>),
+    typeof(LoggingBehavior<,>));
+ */
+
+public sealed class LoggingBehavior<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : notnull
 {
+    private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+
+    public LoggingBehavior(
+        ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken ct)
+    {
+        var requestName = typeof(TRequest).Name;
+
+        _logger.LogInformation(
+            "Handling {RequestName} {@Request}",
+            requestName,
+            request);
+
+        var sw = Stopwatch.StartNew();
+
+        var response = await next();
+
+        sw.Stop();
+
+        _logger.LogInformation(
+            "Handled {RequestName} in {ElapsedMs}ms",
+            requestName,
+            sw.ElapsedMilliseconds);
+
+        return response;
+    }
 }
