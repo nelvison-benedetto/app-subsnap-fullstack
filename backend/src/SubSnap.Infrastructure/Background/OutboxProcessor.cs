54aefc23066bdfc593a3412b65547b1c85a3ed78
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-03-01 09:17:37 +0100

    update

--- backend/src/SubSnap.Infrastructure/Background/OutboxProcessor.cs
+++ backend/src/SubSnap.Infrastructure/Background/OutboxProcessor.cs
@@ -40,36 +40,39 @@ public sealed class OutboxProcessor : BackgroundService
     protected override async Task ExecuteAsync(
         CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
+        while (!stoppingToken.IsCancellationRequested)  //loop infinito che continua a leggere i messaggi dall'outbox finché il servizio è attivo (non viene shutdown dall'user)
         {
-            using var scope = _scopeFactory.CreateScope();
+            using var scope = _scopeFactory.CreateScope();  //crei scope manuale, xk backgrounservice è singleton, e applicationdbcontext è scoped, quindi devi creare scope manuale per risolverlo
 
             var db = scope.ServiceProvider
-                .GetRequiredService<ApplicationDbContext>();
+                .GetRequiredService<ApplicationDbContext>(); //nuovo dbcontext per ogni ciclo, così eviti problemi di concorrenza e garantisci che ogni ciclo lavori con un contesto pulito
 
             var messages = await db.OutboxMessages
                 .Where(x => x.ProcessedOnUtc == null)
                 .Take(20)
                 .ToListAsync(stoppingToken);
+            //lettura outbox, prendi i primi 20 messaggi non processati (processedOnUtc == null), così eviti di sovraccaricare il sistema se ci sono molti messaggi da processare
 
             foreach (var msg in messages)
             {
-                var type = Type.GetType(msg.Type)!;
+                var type = Type.GetType(msg.Type)!;  //!!xk devi salvare AssemblyQualifiedName del tipo, così puoi deserializzare correttamente l'evento di dominio, anche se hai più eventi con lo stesso nome in assembly diversi. non .Name xk altrimenti in produzione fallirà.
 
                 var domainEvent =
                     JsonSerializer.Deserialize(
                         msg.Payload, type);
+                //deserializzazione evento di dominio, così ricostruisci l'evento di dominio originale a partire dal payload JSON salvato nell'outbox
 
                 await _mediator.Publish(
                     (INotification)domainEvent!,
                     stoppingToken);
+                //publish evento di dominio usando MediatR, così sfrutti il meccanismo di gestione degli eventi e i relativi handler che hai definito nella tua applicazione
 
-                msg.ProcessedOnUtc = DateTime.UtcNow;
+                msg.ProcessedOnUtc = DateTime.UtcNow;  //lo marchi
             }
 
             await db.SaveChangesAsync(stoppingToken);
 
-            await Task.Delay(2000, stoppingToken);
+            await Task.Delay(2000, stoppingToken);  //!!POLLING OGNI 2 SEC
         }
     }
 }
