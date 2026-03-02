5589da83a0439da9d4f1e2526fab6660e597cf98
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-03-02 21:49:08 +0100

    update

---
+++ backend/src/SubSnap.Infrastructure/Persistence/Configurations/OutboxMessageConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SubSnap.Infrastructure.Persistence.Outbox;
+
+namespace SubSnap.Infrastructure.Persistence.Configurations;
+
+//see User.cs  transactionbehavior.cs efunitofwork.cs  outboxprocessor.cs outboxmessage.cs
+
+public sealed class OutboxMessageConfiguration
+    : IEntityTypeConfiguration<OutboxMessage>
+{
+    public void Configure(EntityTypeBuilder<OutboxMessage> builder)
+    {
+        builder.ToTable("outboxmessages");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("id");
+
+        builder.Property(x => x.Type)
+            .HasColumnName("type")
+            .IsRequired();
+
+        builder.Property(x => x.Payload)
+            .HasColumnName("payload")
+            .HasColumnType("jsonb")
+            .IsRequired();
+
+        builder.Property(x => x.OccurredOnUtc)
+            .HasColumnName("occurredonutc");
+
+        builder.Property(x => x.ProcessedOnUtc)
+            .HasColumnName("processedonutc");
+    }
+}
