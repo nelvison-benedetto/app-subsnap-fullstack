939efc7ab0fcd42e01431a4f2eefc82f0dffbd82
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-03-11 17:35:17 +0100

    update

---
+++ backend/src/SubSnap.Infrastructure/Caching/QueryCache.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Concurrent;
+
+namespace SubSnap.Infrastructure.Caching;
+
+public class QueryCache
+{
+    private readonly ConcurrentDictionary<string, object> _cache = new();
+    public bool TryGet<T>(string key, out T value)
+    {
+        if (_cache.TryGetValue(key, out var obj) && obj is T typed)
+        {
+            value = typed;
+            return true;
+        }
+
+        value = default!;
+        return false;
+    }
+    public void Set<T>(string key, T value)
+    {
+        _cache[key] = value!;
+    }
+
+}
