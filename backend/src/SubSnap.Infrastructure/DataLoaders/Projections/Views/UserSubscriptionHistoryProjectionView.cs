46d5dcb0ffd644eff27f7f8b74a082b73e71b557
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-03-11 14:02:28 +0100

    update

---
+++ backend/src/SubSnap.Infrastructure/DataLoaders/Projections/Views/UserSubscriptionHistoryProjectionView.cs
@@ -0,0 +1,27 @@
+﻿using SubSnap.Core.Domain.Entities;
+
+namespace SubSnap.Infrastructure.DataLoaders.Projections.Views;
+
+/*
+non va nel Domain, xk è un read models delle query!! non è un business aggregate!
+ */
+public sealed class UserSubscriptionHistoryProjectionView
+{
+    public User User { get; }
+
+    public Subscription Subscription { get; }
+
+    public IReadOnlyCollection<SubscriptionHistory> History { get; }
+
+    public UserSubscriptionHistoryProjectionView(
+        User user,
+        Subscription subscription,
+        IReadOnlyCollection<SubscriptionHistory> history)
+    {
+        User = user ?? throw new ArgumentNullException(nameof(user));
+        Subscription = subscription ?? throw new ArgumentNullException(nameof(subscription));
+        History = history?.ToList().AsReadOnly()
+            ?? throw new ArgumentNullException(nameof(history));
+    }
+
+}
